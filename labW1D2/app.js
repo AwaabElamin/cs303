f1231fbbcfce2fe2dcf5c05a92927e14206ca4bf
Awaab Elamin <AwaabElamin@gmail.com>
2021-03-23 14:05:31 -0500

    x

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,20 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+// See tests.js for behavior
+function nameString(obj) {
+    let result;
+    if (obj === null) return "null argument";
+    if (obj === undefined) return "no object passed";
+    if (Object.keys(obj).length === 0) return "object has no properties";
+    if (Object.keys(obj).length === 1) {
+        for (const key in obj) return key;
+    }
+    result = "";
+    for (const key in obj) {
+        result += key;
+    }
+    return result.split("").sort().join(".");
 }
+console.log(nameString({a:5, z:6, m:8}));
